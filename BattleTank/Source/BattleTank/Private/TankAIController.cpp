77be2956bb36c22b7c5f2c7c387cfc5f9d829605
sappy28 <sparshbhardwaj418@gmail.com>
2018-08-14 21:59:36 +0530

    BT 171

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -25,7 +25,7 @@ void ATankAIController::Tick(float DeltaTime)
 	auto AimingComponent = ControlledTank->FindComponentByClass<UTankAimingComponent>();
 	AimingComponent->AimAt(PlayerTank->GetActorLocation());
 		
-	//ControlledTank->Fire(); 
+	AimingComponent->Fire(); 
 	
 	 
 }
